da0016562771dd273ede7552e8836953a765ef69
Bradley Grantham <bgrantham@channel4.co.uk>
2019-04-17 18:14:30 +0100

    :tada: Initial project files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="thisisnotafortniteskin",
+    maintainer="Bradley Grantham",
+    version="0.0.1",
+    author="Bradley Grantham",
+    author_email="bradley.grantham@bath.edu",
+    description="A GAN which generates Fortnite skins",
+    url="",
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+    ),
+)
